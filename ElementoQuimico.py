f3e5c93b88d7dd01c1695c3910391d83ab62ba8e
Guilherme Pagliarini <guilherme.pagliarini@ima.sp.gov.br>
2017-10-11 18:17:37 -0300

    Criação de Objeto ElementoQuimico

---
+++ ElementoQuimico.py
@@ -0,0 +1,70 @@
+import pygame
+import os, sys
+
+class ElementoQuimico:
+    nomeElemento = ''
+    familia = 0
+    letra = ''
+
+    cor = (0, 0, 0)
+    corTexto = (0, 0, 0)
+
+    fonteLetra = ''
+    fonteNomeElemento = ''
+
+    base = 55
+    altura = 70
+
+    def __init__(self, nomeElemento, familia, letra):
+        pygame.font.init()
+
+        self.nomeElemento = nomeElemento
+        self.familia = familia
+        self.letra = letra
+
+        self.fonteLetra = pygame.font.SysFont('Arial', 40)
+        self.fonteNomeElemento = pygame.font.SysFont('Arial', 12)
+
+        # TRATAMENTO DE CORES
+        metaisAlcalinos = (255, 165, 0)
+        metaisAlcalinosTerrosos = (255, 215, 0)
+        familiaBoro = (72, 209, 204)
+        familiaCarbono = (107, 142, 35)
+        familiaNitrogenio = (34, 139, 34)
+        calcogenios = (72, 61, 139)
+        halogenios = (255, 105, 180)
+        gasesNobres = (65, 105, 225)
+
+        if int(self.familia) == 1:
+            self.cor = metaisAlcalinos
+        elif int(self.familia) == 2:
+            self.cor = metaisAlcalinosTerrosos
+        elif int(self.familia) == 3:
+            self.cor = familiaBoro
+        elif int(self.familia) == 4:
+            self.cor = familiaCarbono
+        elif int(self.familia) == 5:
+            self.cor = metaisAlcalinosTerrosos
+        elif int(self.familia) == 6:
+            self.cor = familiaNitrogenio
+        elif int(self.familia) == 7:
+            self.cor = calcogenios
+        elif int(self.familia) == 8:
+            self.cor = halogenios
+        elif int(self.familia) == 9:
+            self.cor = gasesNobres
+        else:
+            self.cor = (0, 0, 0)
+
+    def desenhaElemento(self, gameDisplay, x, y):
+        posicaoNomeElemento = (x + 2, y + 55)
+        posicaoLetraElemento = (x + 15, y + 10)
+
+        letraElemento = self.fonteLetra.render(self.letra, 1, self.corTexto)
+        letraNomeElemento = self.fonteNomeElemento.render(self.nomeElemento, 1, self.corTexto)
+
+        pygame.draw.rect(gameDisplay, self.cor, [x, y, self.base, self.altura])
+
+        gameDisplay.blit(letraElemento, posicaoLetraElemento)
+        gameDisplay.blit(letraNomeElemento, posicaoNomeElemento)
+
--- PyQuimica.py
+++ PyQuimica.py
@@ -1,25 +1,20 @@
 import pygame
 import os, sys
 
+from ElementoQuimico import ElementoQuimico
+
 pygame.init()
+
+gameDisplay = pygame.display.set_mode((800,600))
+pygame.display.set_caption('PyQuímica')
 
 black = (0,0,0)
 white = (255,255,255)
 green = (0,255,0)
 
-metaisAlcalinos = (255,165,0)
-metaisAlcalinosTerrosos = (255,215,0)
-familiaBoro = (72,209,204)
-familiaCarbono = (107,142,35)
-familiaNitrogenio = (34,139,34)
-calcogenios = (72,61,139)
-halogenios = (255,105,180)
-gasesNobres = (65,105,225)
-
-gameDisplay = pygame.display.set_mode((800,600))
-pygame.display.set_caption('PyQuímica')
-
 gameExit = False
+
+potassio = ElementoQuimico('Potássio', 1, 'K')
 
 while not gameExit:
     for event in pygame.event.get():
@@ -29,12 +24,11 @@ while not gameExit:
 
     gameDisplay.fill(black)
 
-    pygame.draw.rect(gameDisplay, white, [400,300, 10, 100])
-    pygame.draw.rect(gameDisplay, green, [400, 300, 10, 10])
-
+    potassio.desenhaElemento(gameDisplay, 10,10)
     pygame.display.update()
 
 pygame.display.update()
 pygame.quit()
 
 quit()
+
